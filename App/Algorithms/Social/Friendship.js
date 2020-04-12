6b85ea471c96d335ee37a07894d1d7772a0094d7
Nitish Kumar <nkpc14@gmail.com>
2020-04-12 20:43:42 +0530

    Adding Friends

--- App/Algorithms/Social/Friendship.js
+++ App/Algorithms/Social/Friendship.js
@@ -1,3 +1,11 @@
+const Friends = require('../../models/Friends');
+const Users = require('../../models/Users');
+
 class Friendship {
+    Friends = null;
+
+    constructor(FriendsModel) {
+
+    }
 
 }
